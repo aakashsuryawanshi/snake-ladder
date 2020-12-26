b93ca5e5c9cfc4585c43b60a6cff380857ae6949
Akash Suryawanshi <s.akashb@gmail.com>
2020-12-26 15:45:08 +0530

    singleton board with additional code

--- src/main/java/com/as/sl/entities/Board.java
+++ src/main/java/com/as/sl/entities/Board.java
@@ -3,7 +3,12 @@ package com.as.sl.entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.as.sl.constants.CommonConstants;
+import com.as.sl.constants.ExceptionConstants;
 import com.as.sl.entities.jumpers.Jumper;
+import com.as.sl.entities.jumpers.Ladder;
+import com.as.sl.entities.jumpers.Snake;
+import com.as.sl.exception.SLException;
 
 public class Board {
 
@@ -24,5 +29,48 @@ public class Board {
 		return instance;
 	}
 	
+	public void addSanke(int start, int end) {
+		if(!isJumperValid(start, end)) {
+			throw new SLException(ExceptionConstants.INVALID_SNAKE, ExceptionConstants.POSITION_INVALID);
+		}
+		jumpers.add(new Snake(start, end));
+	}
 	
+	public void addLadder(int start, int end) {
+		if(!isJumperValid(start, end)) {
+			throw new SLException(ExceptionConstants.INVALID_LADDER, ExceptionConstants.POSITION_INVALID);
+		}
+		jumpers.add(new Ladder(start, end));
+	}
+	
+	public void printBoard() {
+		Board b = Board.getInstance();
+		System.out.println(CommonConstants.SNAKES_ARE);
+		for(Jumper a : jumpers) {
+			if(a instanceof Snake) {
+				System.out.println(CommonConstants.START + a.getStart() + " " + CommonConstants.END + a.getEnd());
+			}
+		}
+		System.out.println(CommonConstants.LADDERS_ARE);
+		for(Jumper a : jumpers) {
+			if(a instanceof Ladder) {
+				System.out.println(CommonConstants.START + a.getStart() + " " + CommonConstants.END + a.getEnd());
+			}
+		}
+		System.out.println("\n\n");
+	}
+	
+	public boolean isJumperValid(int start, int end) {
+		for(Jumper a : jumpers) {
+			if(a.getStart() == start || a.getEnd() == end) {
+				System.out.println("Conflict "  + CommonConstants.START + a.getStart() + " " + CommonConstants.END + a.getEnd());
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public List<Jumper> getJumpers(){
+		return jumpers;
+	}
 }
